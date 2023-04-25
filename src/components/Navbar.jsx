0a1b08d56ec386d7ffd504f06eca37232bf0eaf8
Casadjes <115717042+Casadjes@users.noreply.github.com>
2023-04-24 19:45:36 -0700

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export function Navbar({ brand }) {
+	return (
+		<nav className='navbar navbar-dark bg-dark'>
+			<div className='container'>
+				<a className='navbar-brand text-uppercase' href='/'>
+					{brand}
+				</a>
+			</div>
+		</nav>
+	);
+}
